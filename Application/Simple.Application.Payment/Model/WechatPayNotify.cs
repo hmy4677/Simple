c6f5cdbfcff4f81e5e73c7e1a55088340642a6d4
何明钰 <hmy4677@qq.com>
2022-07-06 20:21:55 +0800

    支付通知回调,appsetting

---
+++ Application/Simple.Application.Payment/Model/WechatPayNotify.cs
@@ -0,0 +1,68 @@
+﻿namespace Simple.Application.Payment.Model;
+
+/// <summary>
+/// 微信回调通知model
+/// </summary>
+public class WechatPayNotify
+{
+    /// <summary>
+    /// 通知ID
+    /// </summary>
+    public string id { get; set; }
+
+    /// <summary>
+    /// 通知创建时间
+    /// </summary>
+    public string create_time { get; set; }
+
+    /// <summary>
+    /// 通知类型
+    /// </summary>
+    public string event_type { get; set; }
+
+    /// <summary>
+    /// 通知数据类型
+    /// </summary>
+    public string resource_type { get; set; }
+
+    /// <summary>
+    /// 回调摘要
+    /// </summary>
+    public string summary { get; set; }
+
+    /// <summary>
+    /// 通知数据
+    /// </summary>
+    public ResourceInfo resource { get; set; }
+}
+
+/// <summary>
+/// 通知数据
+/// </summary>
+public class ResourceInfo
+{
+    /// <summary>
+    /// 加密算法类型
+    /// </summary>
+    public string algorithm { get; set; }
+
+    /// <summary>
+    /// 数据密文
+    /// </summary>
+    public string ciphertext { get; set; }
+
+    /// <summary>
+    /// 附加数据
+    /// </summary>
+    public string associated_data { get; set; }
+
+    /// <summary>
+    /// 原始类型
+    /// </summary>
+    public string original_type { get; set; }
+
+    /// <summary>
+    /// 随机串
+    /// </summary>
+    public string nonce { get; set; }
+}
--- Web/Simple.Web/Controllers/Base/BaseController.cs
+++ Web/Simple.Web/Controllers/Base/BaseController.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
+using Simple.Application.Payment.Interface;
+using Simple.Application.Payment.Model;
 using Simple.Services.DbInitial.Interface;
+using System.Text.Json;
 
 namespace Simple.Web.Controllers.Base;
 /// <summary>
@@ -11,32 +14,135 @@ namespace Simple.Web.Controllers.Base;
 [Route("api/[controller]")]
 public class BaseController : Controller
 {
-  private readonly IDbInitial _dbInitial;
-  private readonly IMemoryCache _memory;
-  private const string _timeCacheKey = "timeKey";
+    private readonly IDbInitial _dbInitial;
+    private readonly IMemoryCache _memory;
+    private readonly ISecurity _security;
+    private const string _timeCacheKey = "timeKey";
 
-  public BaseController(IDbInitial dbInitial, IMemoryCache memory)
-  {
-    _dbInitial = dbInitial;
-    _memory = memory;
-  }
-
-  /// <summary>
-  /// 初始化数据库
-  /// </summary>
-  [HttpGet("init_db")]
-  public void InitDB()
-  {
-    _dbInitial.CreateDbAndTable();
-  }
-
-  [HttpGet("time")]
-  public string UseMemoryCache()
-  {
-    return _memory.GetOrCreate(_timeCacheKey, entry =>
+    public BaseController(IDbInitial dbInitial, IMemoryCache memory, ISecurity security)
     {
-      entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(100);
-      return DateTime.Now.ToString("T");
-    });
-  }
+        _dbInitial = dbInitial;
+        _memory = memory;
+        _security = security;
+    }
+
+    /// <summary>
+    /// 初始化数据库
+    /// </summary>
+    [HttpGet("init_db")]
+    public void InitDB()
+    {
+        _dbInitial.CreateDbAndTable();
+    }
+
+    /// <summary>
+    /// 内存缓存使用示例
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("time")]
+    public string UseMemoryCache()
+    {
+        return _memory.GetOrCreate(_timeCacheKey, entry =>
+        {
+            entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(100);
+            return DateTime.Now.ToString("T");
+        });
+    }
+
+    /// <summary>
+    /// 支付宝付款通知回调
+    /// </summary>
+    /// <returns></returns>
+    [AllowAnonymous]
+    [HttpPost("alipay_notify")]
+    public async Task<IActionResult> AliNotify()
+    {
+        var sArray = GetRequestPost();
+
+        string out_trade_no = sArray["out_trade_no"];
+        string trade_no = sArray["trade_no"];
+        string total_amount = sArray["total_amount"];
+
+        if (sArray.Count != 0)
+        {
+            //验签
+            bool flag = _security.VerifyAli(sArray);
+            if (!flag)
+            {
+                return BadRequest();
+            }
+            //验签成功操作
+            //...
+
+            return Ok();
+        }
+        return BadRequest();
+    }
+
+    /// <summary>
+    /// 微信付款通知回调
+    /// </summary>
+    /// <returns></returns>
+    [AllowAnonymous]
+    [HttpPost("wechat_notify")]
+    public async Task<IActionResult> WechatNotify()
+    {
+        //获取body
+        var smr = new StreamReader(Request.Body);
+        var bodyjson = await smr.ReadToEndAsync();
+        if (string.IsNullOrEmpty(bodyjson))
+        {
+            var error = new
+            {
+                code = "FAIL",
+                message = "内容为空"
+            };
+            return BadRequest(JsonSerializer.Serialize(error));
+        }
+        //验签
+        var signature = Request.Headers["Wechatpay-Signature"];
+        var stamp = Request.Headers["Wechatpay-Timestamp"];
+        var nonce = Request.Headers["Wechatpay-Nonce"];
+        var verify = _security.VerifyWechat(signature, stamp, nonce, bodyjson);
+        if (!verify)
+        {
+            var error = new
+            {
+                code = "FAIL",
+                message = "验签失败"
+            };
+            return BadRequest(JsonSerializer.Serialize(error));
+        }
+
+        //解密
+        var notify = JsonSerializer.Deserialize<WechatPayNotify>(bodyjson);
+        var decrypt =JsonSerializer.Deserialize<WechatPayQueryResponse>(
+            _security.AesGcmDecrypt(notify.resource.associated_data, notify.resource.nonce, notify.resource.ciphertext));
+
+        if (decrypt.trade_state_desc != "支付成功")
+        {
+            var error = new
+            {
+                code = "FAIL",
+                message = decrypt.trade_state_desc
+            };
+            return BadRequest(JsonSerializer.Serialize(error));
+        }
+        //更新订单数据
+        //...
+
+        return Ok();
+    }
+
+    [NonAction]
+    private Dictionary<string, string> GetRequestPost()
+    {
+        var sArray = new Dictionary<string, string>();
+        var coll = Request.Form;
+        foreach (var item in coll)
+        {
+            sArray.Add(item.Key, item.Value);
+        }
+        return sArray;
+    }
 }
